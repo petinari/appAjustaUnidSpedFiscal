1270997a4617fbabf865fccd9ac9ba89a0a7c655
robsonpetinari <Petinari@outlook.com>
2022-02-18 16:52:13 -0400

    1

--- appAjustaUnidSpedFiscal/main.cs
+++ appAjustaUnidSpedFiscal/main.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
 
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        var arquivo = new List<String>(System.IO.File.ReadLines(@"/Users/robsonpetinari/Documents/EFD_00094_00001.txt"));
+        var _0200List = new List<String>();
+        var _C170List = new List<String>();
+        int erros = 0;
+
+        foreach (string line in arquivo)
+        {
+            if (line.Contains("|0200|"))
+            {
+                _0200List.Add(line);
+            }
+            if (line.Contains("|C170|"))
+            {
+                _C170List.Add(line);
+            }
+        }
+        foreach (var item0200 in _0200List)
+        {
+            var linha0200 = item0200.Split('|');
+            int index = 0;
+            foreach (var itemC170 in _C170List)
+            {
+                var linhaC170 = itemC170.Split('|');
+                if (linha0200[2] == linhaC170[3])
+                {
+                    if (linha0200[6] != linhaC170[6])
+                    {
+                        List<String> linhaC170Novo = new List<String>(linhaC170);
+                        linhaC170Novo[6] = linha0200[6];
+                        var indexC170 = arquivo.IndexOf(itemC170);
+                        arquivo[indexC170] = String.Join("|", linhaC170Novo);
+                        erros++;
+                    }
+                }
+                index++;
+            }
+        }
+        Console.WriteLine(erros);
+        System.IO.File.WriteAllLines(@"/Users/robsonpetinari/Documents/EFD.txt", arquivo);
+    }
+}
